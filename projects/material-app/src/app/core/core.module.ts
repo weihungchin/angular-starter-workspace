5e36e8163b2ab175909630270d5693d165e57605
WeiHung <wei-hung.chin@edag.com.my>
2022-09-30 01:37:39 +0000

    Add angular material and init toolbar component

--- projects/material-app/src/app/core/core.module.ts
+++ projects/material-app/src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { GlobalLayoutComponent } from './layout/global-layout/global-layout.component';
-
 @NgModule({
-  declarations: [
-    GlobalLayoutComponent
+  declarations: [GlobalLayoutComponent],
+  imports: [
+    // angular
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    // third party
+    MatToolbarModule,
+    MatButtonModule,
   ],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  exports: [GlobalLayoutComponent],
 })
 export class CoreModule {}
