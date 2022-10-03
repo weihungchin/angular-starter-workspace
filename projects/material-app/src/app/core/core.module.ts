77c182f59fea50ad0288176fd028dff489751915
WeiHung <wei-hung.chin@edag.com.my>
2022-10-03 01:28:13 +0000

    Add custom material theming

--- projects/material-app/src/app/core/core.module.ts
+++ projects/material-app/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
@@ -15,6 +16,7 @@ import { GlobalLayoutComponent } from './layout/global-layout/global-layout.comp
     // third party
     MatToolbarModule,
     MatButtonModule,
+    MatSlideToggleModule,
   ],
   exports: [GlobalLayoutComponent],
 })
--- projects/material-app/src/app/core/layout/global-layout/global-layout.component.ts
+++ projects/material-app/src/app/core/layout/global-layout/global-layout.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-global-layout',
   templateUrl: './global-layout.component.html',
-  styleUrls: ['./global-layout.component.scss']
+  styleUrls: ['./global-layout.component.scss'],
 })
 export class GlobalLayoutComponent implements OnInit {
+  color: ThemePalette = 'accent';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
